1c060c21ce62ae2f7416f7a2dc08175f92feaffb
Tului Gantulga <tgantulga@ucdavis.edu>
2021-06-09 16:56:00 -0700

    Initial Commit

---
+++ Finstagram/FinstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinstagramApp.swift
+//  Finstagram
+//
+//  Created by Tului Gantulga on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct FinstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
