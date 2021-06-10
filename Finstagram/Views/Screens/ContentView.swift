4564d20c6da87d2da7b2156302b4476d9e7a1fe7
Tului Gantulga <tgantulga@ucdavis.edu>
2021-06-09 17:37:30 -0700

    tabs setup

---
+++ Finstagram/Views/Screens/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Finstagram
+//
+//  Created by Tului Gantulga on 6/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("Screen1")
+                .tabItem {
+                    Image(systemName: "book.fill")
+                    Text("Feed")
+                }
+            
+            Text("Screen2")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Browse")
+                }
+            
+            Text("Screen3")
+                .tabItem {
+                    Image(systemName: "square.and.arrow.up.fill")
+                    Text("Upload")
+                }
+            
+            Text("Screen4")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
+            
+            
+            
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
