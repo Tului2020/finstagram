3d32955cc4becc4f377e72045cfda7dbe62a26d2
Tului Gantulga <tgantulga@ucdavis.edu>
2021-06-09 17:57:28 -0700

    template for a post is done

--- Finstagram/Views/Subviews/PostView.swift
+++ Finstagram/Views/Subviews/PostView.swift
@@ -31,10 +31,34 @@ struct PostView: View {
             .padding(.all, 6)
             
             
-            // MARK : IMAGE
+            // MARK: IMAGE
             Image("dog1")
                 .resizable()
                 .scaledToFit()
+            
+            // MARK: FOOTER
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20, content: {
+                Image(systemName: "heart")
+                    .font(.title3)
+                
+                Image(systemName: "bubble.middle.bottom")
+                    .font(.title3)
+                
+                Image(systemName: "paperplane")
+                    .font(.title3)
+                
+                Spacer()
+                
+            })
+            .padding(.all, 6)
+            
+            
+            
+            HStack {
+                Text("This is the caption for the photo")
+                Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+            }
+            .padding(.all, 6)
             
             
         })
