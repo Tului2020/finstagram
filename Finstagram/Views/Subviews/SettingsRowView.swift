b5650fc72c5b8aecda20c22f8b46366999231faa
Tului Gantulga <tgantulga@ucdavis.edu>
2021-06-18 18:25:59 -0700

    made progress on settings page

---
+++ Finstagram/Views/Subviews/SettingsRowView.swift
@@ -0,0 +1,47 @@
+//
+//  SettingsRowView.swift
+//  Finstagram
+//
+//  Created by Tului Gantulga on 6/18/21.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    var leftIcon: String;
+    var text: String;
+    var color: Color;
+    
+    
+    var body: some View {
+        HStack {
+        
+            ZStack {
+                RoundedRectangle(cornerRadius: 8, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
+                    .fill(color)
+                
+                
+                Image(systemName: leftIcon)
+                    .font(.title3)
+                    .foregroundColor(.white)
+            }
+            .frame(width: 36, height: 36, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
+            Text(text)
+            Spacer()
+            Image(systemName: "chevron.right")
+                .font(.headline)
+            
+            
+        }
+        .padding(.vertical, 4)
+        
+    }
+}
+
+struct SettingsRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsRowView(leftIcon: "heart.fill", text: "row title", color: Color.red)
+            .previewLayout(.sizeThatFits)
+    }
+}
